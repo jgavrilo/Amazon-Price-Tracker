7417fa15845b13a3df91f42417c04c448edbc2c8
Jeremy Gavrilov <51212746+jgavrilo@users.noreply.github.com>
2023-04-15 18:36:45 -0700

    Added comments to both files

--- amazon_config.py
+++ amazon_config.py
@@ -1,28 +1,42 @@
+# Import the webdriver module from the Selenium library
 from selenium import webdriver
 
-#Change mame, currency, and price range to fit needs
+# Specify the directory where the program will save the generated reports
 DIRECTORY = 'reports'
+
+# Set the name of the product to search for, the currency symbol used in the search,
+# and the minimum and maximum price range to search within, respectively
 NAME = "ipad pro"
 CURRENCY = "$"
 MIN_PRICE = "500"
 MAX_PRICE = "1100"
+
+# Create a dictionary to set the price range filter for the Amazon search
 FILTERS = {
     'min': MIN_PRICE,
     'max': MAX_PRICE
 }
+
+# Set the URL for the Amazon website
 BASE_URL = "http://www.amazon.com/"
 
+# Define a function to return a webdriver instance with Chrome browser options passed as an argument
 def get_chrome_web_driver(options):
     return webdriver.Chrome("./chromedriver", chrome_options=options)
 
+# Define a function to return an instance of webdriver.ChromeOptions()
 def get_web_driver_options():
     return webdriver.ChromeOptions()
 
+# Define a function to set the --ignore-certificate-errors flag to ignore any SSL certificate errors
 def set_ignore_certificate_error(options):
     options.add_argument('--ignore-certificate-errors')
 
+# Define a function to set the --incognito flag to launch the browser in incognito mode
 def set_browser_as_incognito(options):
     options.add_argument('--incognito')
 
+# Define a function to set the --headless flag to launch the browser in headless mode,
+# which means that it will run in the background without displaying a browser window
 def set_automation_as_head_less(options):
     options.add_argument('--headless')
--- simple_tracker.py
+++ simple_tracker.py
@@ -1,3 +1,4 @@
+# Import necessary libraries and classes
 import time
 from selenium.webdriver.common.keys import Keys
 from amazon_config import (
@@ -12,12 +13,11 @@ from amazon_config import (
     BASE_URL,
     DIRECTORY
 )
-
 from selenium.common.exceptions import NoSuchElementException
 import json
 from datetime import datetime
 
-
+# Define a class for generating a report
 class GenerateReport:
     def __init__(self, file_name, filters, base_link, currency, data):
         self.file_name = file_name
@@ -35,64 +35,73 @@ class GenerateReport:
             'products': self.data
         }
         print("Creating report for item...")
+        # Write the report to a JSON file
         with open(f'{DIRECTORY}/{file_name}.json', 'w') as f:
             json.dump(report, f)
         print("...Done")
 
     @staticmethod
     def get_now():
+        # Get the current date and time
         now = datetime.now()
         return now.strftime("%d/%m/%Y %H:%M:%S")
 
     def get_best_item(self):
         try:
+            # Sort the product data by price and return the best (i.e., cheapest) item
             return sorted(self.data, key=lambda k: k['price'])[0]
         except Exception as e:
             print(e)
-            print("A problem occured while sorting the items")
+            print("A problem occurred while sorting the items")
             return None
 
+# Define a class for interacting with the Amazon website using Selenium
 class AmazonAPI:
     def __init__(self, search_term, filters, base_url, currency):
         self.base_url = base_url
         self.search_term = search_term
+        # Set up Chrome webdriver options
         options = get_web_driver_options()
         set_ignore_certificate_error(options)
         set_browser_as_incognito(options)
+        # Set up Chrome webdriver with options
         self.driver = get_chrome_web_driver(options)
         self.currency = currency
+        # Set up price filter for Amazon search
         self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
 
     def run(self):
         print("Starting Script...")
         print(f"Looking for {self.search_term} products...")
+        # Get product links for the specified search term
         links = self.get_product_links()
         if not links:
             print("Stop")
             return
         print(f"Got {len(links)} product links")
         print("Getting info about them...")
+        # Get product data for each product link
         products = self.get_products_info(links)
         print(f"Got info about {len(products)} products...")
+        # Quit the Chrome webdriver instance
         self.driver.quit()
         return products
 
     def get_product_links(self):
-        #Opens webpage
+        # Open the Amazon website
         self.driver.get(self.base_url)
-
-        #Puts the item in the search bar
+        # Enter the search term in the search bar
         element = self.driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
         element.send_keys(self.search_term)
         element.send_keys(Keys.ENTER)
         time.sleep(2)
-
-        #Add in the price filter
+        # Add in the price filter to the search
         self.driver.get(f'{self.driver.current_url}{self.price_filter}')
         print(f"The URL: {self.driver.current_url}")
         time.sleep(2)
-
-        #Get the links for each item on the page and return them
+        print(f"The URL: {self.driver.current_url}")
+        time.sleep(2)
+        # Get the links for each product on the page and return them
         result_list = self.driver.find_elements_by_class_name('s-result-list')
         links = []
         try:
@@ -108,6 +117,7 @@ class AmazonAPI:
     def get_products_info(self, links):
         asins = self.get_asins(links)
         products = []
+        # For each product link, get its product data and append it to the products list
         for asin in asins:
             product = self.get_single_product_info(asin)
             if product:
@@ -115,17 +125,21 @@ class AmazonAPI:
         return products
 
     def get_asins(self, links):
+        # Get the ASIN for each product link
         return [self.get_asin(link) for link in links]
 
 
     def get_single_product_info(self, asin):
         print(f"Product ID: {asin} - getting data")
+        # Get the product data for a single product with the specified ASIN
         product_short_url = self.shorten_url(asin)
         self.driver.get(f'{product_short_url}?language=en_GB')
         time.sleep(2)
+        # Get the product title, seller, and price
         title = self.get_title()
         seller = self.get_seller()
         price = self.get_price()
+        # If all the required product data is available, create a product_info dictionary and return it
         if title and seller and price:
             product_info = {
                 'asin': asin,
@@ -138,6 +152,7 @@ class AmazonAPI:
         return None
 
     def get_title(self):
+        # Get the title of a product
         try:
             return self.driver.find_element_by_id('productTitle').text
         except Exception as e:
@@ -146,6 +161,7 @@ class AmazonAPI:
             return None
 
     def get_seller(self):
+        # Get the seller of a product
         try:
             return self.driver.find_element_by_id('bylineInfo').text
         except Exception as e:
@@ -154,13 +170,16 @@ class AmazonAPI:
             return None
 
     def get_price(self):
+        # Get the price of a product
         price = None
         try:
+            # Check for the price in the main price block
             price = self.driver.find_element_by_id('priceblock_ourprice').text
             price = self.convert_price(price)
         except NoSuchElementException:
             try:
-                availablity = self.driver.find_element_by_id('availablity').tetx
+                # Check for the price in the alternate price block if the main price block is not found
+                availablity = self.driver.find_element_by_id('availablity').text
                 if 'Available' in availablity:
                     price = self.driver.find_element_by_class_name('olp-padding-right').text
                     price = price[price.find(self.currency):]
@@ -176,13 +195,16 @@ class AmazonAPI:
         return price
 
     def shorten_url(self, asin):
+        # Shorten the product URL using the ASIN
         return self.base_url + 'dp/' + asin
     
     @staticmethod
     def get_asin(product_link):
+        # Get the ASIN for a product from its URL
         return product_link[product_link.find('/dp/') + 4:product_link.find('/ref')]
 
     def convert_price(self, price):
+        # Convert the product price to a float
         price = price.split(self.currency)[1]
         try:
             price = price.split("\n")[0] + "." + price.split("\n")[1]
@@ -195,6 +217,8 @@ class AmazonAPI:
         return float(price)
 
 if __name__ == '__main__':
+    # Create an instance of AmazonAPI and run it to get product data
     am = AmazonAPI(NAME, FILTERS, BASE_URL, CURRENCY)
     data = am.run()
+    # Use the product data to generate a report
     GenerateReport(NAME, FILTERS, BASE_URL, CURRENCY, data)
